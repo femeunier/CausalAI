6c3414c1cf6bcfb9d52b602acea09cdb9d1ef56c
femeunier <felicien.meunier@gmail.com>
2025-08-22 20:56:40 +0200

    Num not alpha

--- R/run.Granger.R
+++ R/run.Granger.R
@@ -56,7 +56,7 @@ run.Granger <- function(config.file){
                                                ".*.tif$"),
                               full.names = TRUE)
   climate <- rast(climate.files)
-  names(climate) <- gsub("[._]+$", "",gsub("[[:alnum:]]", "", names(climate)))
+  names(climate) <- gsub("[._]+$", "",gsub("[0-9]","", names(climate)))
   climate.years <- as.numeric(unlist(lapply(strsplit(tools::file_path_sans_ext(basename(climate.files)),"_|\\."),"[[",2)))
   climate.months <- as.numeric(unlist(lapply(strsplit(tools::file_path_sans_ext(basename(climate.files)),"_|\\."),"[[",3)))
 
@@ -67,7 +67,7 @@ run.Granger <- function(config.file){
                                                ".*.tif$"),
                           full.names = TRUE)
   msl <- rast(msl.files)
-  names(msl) <- gsub("[._]+$", "",gsub("[[:alnum:]]", "", names(msl)))
+  names(msl) <- gsub("[._]+$", "",gsub("[0-9]", "", names(msl)))
   msl.years <- as.numeric(unlist(lapply(strsplit(tools::file_path_sans_ext(basename(msl.files)),"_|\\."),"[[",3)))
   msl.months <- as.numeric(unlist(lapply(strsplit(tools::file_path_sans_ext(basename(msl.files)),"_|\\."),"[[",4)))
 
@@ -77,7 +77,7 @@ run.Granger <- function(config.file){
                                            ".*.tif$"),
                          full.names = TRUE)
   cc <- rast(cc.files)
-  names(cc) <- gsub("[._]+$", "",gsub("[[:alnum:]]", "", names(cc)))
+  names(cc) <- gsub("[._]+$", "",gsub("[0-9]","", names(cc)))
   cc.years <- as.numeric(unlist(lapply(strsplit(tools::file_path_sans_ext(basename(cc.files)),"_|\\."),"[[",3)))
   cc.months <- as.numeric(unlist(lapply(strsplit(tools::file_path_sans_ext(basename(cc.files)),"_|\\."),"[[",4)))
 
